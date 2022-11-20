de66b49c492d3e36c364a283dcc78638c6eec097
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-11-20 13:17:31 -0500

    'Works'

--- src/main/java/com/boocrun/web/DemoController.java
+++ src/main/java/com/boocrun/web/DemoController.java
@@ -1,12 +1,16 @@
 package com.boocrun.web;
 
+
 //import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.boocrun.domain.User;
 import com.boocrun.repository.UserRepository;
@@ -38,4 +42,20 @@ public class DemoController {
 		
 		return "redirect:/";
 	}
+//Delete user
+//	 @PostMapping("/")
+//	    public String rootDelete(User user){
+//	        userRepo.delete(user);
+//	        return "redirect:/";
+//	    }
+	 
+//	@RequestMapping(value="/del/{luser}", method=RequestMethod.GET)
+//	public String deleteUser (@PathVariable User luser, ModelMap model)
+//	{
+//		//model.remove("allUsers", allUsers);
+//		model.remove("user", luser);
+////		((HashMap<String, Object>) userRepo).remove(user);
+//		root(model);
+//		return "welcome";
+//	}
 }
